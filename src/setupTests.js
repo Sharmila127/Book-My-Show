8c14f92930a263ae3653090f5775539b947111a1
Sharmila127 <sharmila6201@gmail.com>
2025-09-17 17:16:42 +0530

    Update setupTests.js

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,10 +1,10 @@
-// src/setupTests.js
 import '@testing-library/jest-dom';
 
-// Polyfill for Node 18+ (React Testing Library expects setImmediate/clearImmediate)
+// Polyfill setImmediate/clearImmediate for Node 18+
 if (typeof global.setImmediate === 'undefined') {
   global.setImmediate = (fn, ...args) => setTimeout(fn, 0, ...args);
 }
+
 if (typeof global.clearImmediate === 'undefined') {
   global.clearImmediate = (id) => clearTimeout(id);
 }
