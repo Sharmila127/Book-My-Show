0986ca056962cc013dec621f862ab66288a24862
Sharmila127 <sharmila6201@gmail.com>
2025-09-17 17:11:40 +0530

    Update setupTests.js

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,8 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// 🔹 Fix: polyfill for Node >= 18 where clearImmediate is missing
+if (typeof global.clearImmediate === "undefined") {
+  global.clearImmediate = (fn) => clearTimeout(setImmediate(fn));
+}
