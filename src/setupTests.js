82c538263d61b17fcd1e95a9112ebcc2479f78dd
Sharmila127 <sharmila6201@gmail.com>
2025-09-17 17:15:51 +0530

    Update setupTests.js

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,10 +1,10 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
+// src/setupTests.js
 import '@testing-library/jest-dom';
 
-// 🔹 Fix: polyfill for Node >= 18 where clearImmediate is missing
-if (typeof global.clearImmediate === "undefined") {
-  global.clearImmediate = (fn) => clearTimeout(setImmediate(fn));
+// Polyfill for Node 18+ (React Testing Library expects setImmediate/clearImmediate)
+if (typeof global.setImmediate === 'undefined') {
+  global.setImmediate = (fn, ...args) => setTimeout(fn, 0, ...args);
+}
+if (typeof global.clearImmediate === 'undefined') {
+  global.clearImmediate = (id) => clearTimeout(id);
 }
